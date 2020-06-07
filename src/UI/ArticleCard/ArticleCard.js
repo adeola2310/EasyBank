46fa90a0209afe2dccef60b4afddec22f66a8c61
adeola2310 <adekoyejoadeola@gmail.com>
2020-06-07 18:33:18 +0100

    some functionalities added

--- src/UI/ArticleCard/ArticleCard.js
+++ src/UI/ArticleCard/ArticleCard.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import './ArtcleCard.css';
-import propTypes from 'prop-types';
+// import propTypes from 'prop-types';
 
 class ArticleCard extends Component {
     render() {
         return (
             <div className="art_card">
-                <img src={this.props.src} className="art_img"/>
+                <img src={this.props.image} className="art_img"/>
             <div className="content">
         <p className="author">{this.props.author}</p>
         <h5 className="title">{this.props.title}</h5>
@@ -18,11 +18,12 @@ class ArticleCard extends Component {
     }
 }
 
-ArticleCard.propTypes = {
-    author: propTypes.string,
-    title: propTypes.string,
-    text: propTypes.string,
-    src: propTypes.string
-};
+// commented cos i dont really need it
+// ArticleCard.propTypes = {
+//     author: propTypes.string,
+//     title: propTypes.string,
+//     text: propTypes.string,
+//     image: propTypes.string
+// };
 
 export default ArticleCard;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,25 +2,50 @@ import React, {Component} from 'react';
 import './Header.css';
 import '../../UI/button/button';
 import Button from "../../UI/button/button";
+import navIcon from "../../images/icon-hamburger.svg"
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.showList = this.showList.bind(this);
+       this.state = {
+           list: false
+       }
+    }
+
+
+    showList =()=>{
+        this.setState({list: true});
+    }
+
+
     render() {
         return (
-            <div className="header">
-               <nav className="header__nav">
-                   <div className="logo">
-                       <img src={require('../../images/logo.svg')} alt="logo"/>
-                   </div>
-                   <ul className="lists">
-                       <li className="item">Home</li>
-                       <li className="item">About</li>
-                       <li className="item">Contact</li>
-                       <li className="item">Blog</li>
-                   </ul>
-                       <Button label="Request Invite"/>
-                       <img src={require('../../images/icon-hamburger.svg')} className="harmbugger" alt="harmburger_icon"/>
-               </nav>
-            </div>
+            <>
+                <div className="header">
+                    <img src={require('../../images/logo.svg')} alt="logo"/>
+                    <nav className="header__nav">
+                        <ul className={this.state.list ? 'lists' : ''}>
+                            <li className="item">Home</li>
+                            <li className="item">Home</li>
+                            <li className="item">Home</li>
+                            <li className="item">Home</li>
+                        </ul>
+
+                        {/*<ul className="lists2">*/}
+                        {/*    <li className="item">Home</li>*/}
+                        {/*    <li className="item">Home</li>*/}
+                        {/*    <li className="item">Home</li>*/}
+                        {/*    <li className="item">Home</li>*/}
+                        {/*</ul>*/}
+
+                    </nav>
+                    {/*<Button label="request invite" className="me"/>*/}
+                    <button className="me">request invite</button>
+                    <img src={navIcon} className="harmbugger" onClick={this.showList}
+                         alt="harmburger_icon"/>
+                </div>
+            </>
         );
     }
 }
